eccdf7cc91d81e86461cadf8eafc16547ac2b58c
E0ho <110147055+E0ho@users.noreply.github.com>
2024-04-15 11:21:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.92 ms, Memory: 26.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,24 @@
+# 한 사람만 완주 x
+# participant : 참여한 선수 이름 리스트
+# completion : 완주한 선수 이름 리스트
+# 완주하지 못한 선수 출력
+
+def solution(participant, completion):
+    answer = ''
+    
+    # 이름 순으로 정렬
+    participant.sort()
+    completion.sort()
+    
+    # 가장 마지막 사람 초기 조건
+    answer = participant[-1]
+    
+    # 참가자 -> 도착 했는지 확인
+    for i in range(len(completion)):
+        
+        # 참가자가 완주자에 없는 경우
+        if participant[i] != completion[i]:
+            answer = participant[i]
+            break
+            
+    return answer
