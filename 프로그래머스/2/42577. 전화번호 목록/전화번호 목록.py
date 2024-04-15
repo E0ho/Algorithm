a34931fa59e88048b8938befac658a787d3eb44d
E0ho <110147055+E0ho@users.noreply.github.com>
2024-04-15 14:32:44 +0900

    [level 2] Title: 전화번호 목록, Time: 318.98 ms, Memory: 34.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,25 @@
+# 번호가 다른 번호의 접두어인 경우
+
+def solution(phone_book):
+    answer = True
+    
+    n = len(phone_book)
+    
+    # 문자 개수로 정렬
+    phone_book.sort(key = len)
+    
+    # 사전 만들기
+    dic = {}
+    for number in phone_book:
+        dic[number] = number
+    
+    # 접두어 확인
+    for number in phone_book:
+        s = ""
+        for num in number:
+            s += num
+            if s in dic.keys() and s != number:
+                answer = False
+                return answer
+            
+    return answer
