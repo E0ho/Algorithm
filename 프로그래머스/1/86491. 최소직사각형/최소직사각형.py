94c065a106bdefdc7b45d11a34db279c85239670
E0ho <110147055+E0ho@users.noreply.github.com>
2024-04-15 10:46:37 +0900

    [level 1] Title: 최소직사각형, Time: 5.60 ms, Memory: 12.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,22 @@
+# 지갑의 크기
+# 모든 명함을 수납할 수 있는 최소 크기의 지갑
+# 가로, 세로 길이
+def solution(sizes):
+    answer = 0
+    
+    # 가로 길이를 무조건 긴변으로 돌리기
+    li = []
+    for a, b in sizes:
+        li.append( (max(a, b), min(a, b)) )
+        
+    width_li = []
+    height_li = []
+    
+    # 가로 세로 배열 만들기
+    for width, height in li:
+        width_li.append(width)
+        height_li.append(height)
+    
+    # 각 길이의 최대 값
+    answer = max(width_li) * max(height_li)
+    return answer
