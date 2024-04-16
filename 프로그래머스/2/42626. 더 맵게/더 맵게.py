8e428c7669dbd7302eede800a328c3d389f0c80b
E0ho <110147055+E0ho@users.noreply.github.com>
2024-04-16 15:54:23 +0900

    [level 2] Title: 더 맵게, Time: 2170.20 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,32 @@
+# 스코빌 지수 k이상으로 만들기
+# 가장 낮은 두개의 음식을 섞기 
+# (가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+# 모든 음식의 스코빌 지수가 K이상일때 까지 섞기
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    
+    # 최소 힙으로 변환
+    heapq.heapify(scoville)
+    
+    # 반복
+    while True:
+        
+        # 모든 음식의 스코빌 지수 K 이상 -> 종료
+        if scoville[0] >= K:
+            return answer
+        
+        # 모든 음식의 스코빌 지수 k 이상 불가
+        if len(scoville) == 1:
+            return -1
+        
+        # 음식 섞기
+        answer += 1
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+
+        cal = first + (second * 2)
+        heapq.heappush(scoville, cal)
+
+    return answer
