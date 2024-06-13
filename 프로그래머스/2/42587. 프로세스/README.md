fabb84568a92d2ce3ccb64e902537e57a512dea0
E0ho <110147055+E0ho@users.noreply.github.com>
2024-06-14 05:04:52 +0900

    [level 2] Title: 프로세스, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.15 ms
+메모리: 10.2 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 16일 14:29:46
+2024년 06월 14일 05:04:50
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -7,26 +7,50 @@ from collections import deque
 def solution(priorities, location):
     answer = 0
     
-    # deque (idx, value)
-    priorities = deque(enumerate(priorities))
+    li = []
+    for i, pri in enumerate(priorities):
+        li.append((i, pri))
+    
+    q = deque(li)
+    count = 0
     
     while True:
         
-        # 시작점
-        start = priorities[0][1]
+        i, pri = q.popleft()
         
-        # 최대값
-        max_value = max([x[1] for x in priorities])
-        
-        # 시작점 우선순위가 가장 높은 경우 실행
-        if start == max_value:
-            target = priorities.popleft()
-            answer += 1
-            if target[0] == location:
-                return answer
-
-        # 시작점 우선순위가 낮은 경우 회전
+        if pri == max(priorities):
+            count += 1
+            priorities.remove(pri)
+            if i == location:
+                return count
+            
         else:
-            priorities.rotate(-1)
+            q.append((i, pri))
+        
+        
         
     return answer
+
+
+
+# # deque (idx, value)
+#     priorities = deque(enumerate(priorities))
+    
+#     while True:
+        
+#         # 시작점
+#         start = priorities[0][1]
+        
+#         # 최대값
+#         max_value = max([x[1] for x in priorities])
+        
+#         # 시작점 우선순위가 가장 높은 경우 실행
+#         if start == max_value:
+#             target = priorities.popleft()
+#             answer += 1
+#             if target[0] == location:
+#                 return answer
+
+#         # 시작점 우선순위가 낮은 경우 회전
+#         else:
+#             priorities.rotate(-1)
