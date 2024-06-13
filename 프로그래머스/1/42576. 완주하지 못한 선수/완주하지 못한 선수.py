ee9797e119e0e7559bd5aa16d111e6cebc9b8f33
E0ho <110147055+E0ho@users.noreply.github.com>
2024-06-14 03:31:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 62.95 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,23 +2,33 @@
 # participant : 참여한 선수 이름 리스트
 # completion : 완주한 선수 이름 리스트
 # 완주하지 못한 선수 출력
+from collections import Counter
 
 def solution(participant, completion):
     answer = ''
+    a = Counter(participant)
+    b = Counter(completion)
+
+    a.subtract(b)
     
-    # 이름 순으로 정렬
-    participant.sort()
-    completion.sort()
+    a = list(a.most_common(1))
     
-    # 가장 마지막 사람 초기 조건
-    answer = participant[-1]
-    
-    # 참가자 -> 도착 했는지 확인
-    for i in range(len(completion)):
-        
-        # 참가자가 완주자에 없는 경우
-        if participant[i] != completion[i]:
-            answer = participant[i]
-            break
-            
+    answer = a[0][0]
     return answer
+
+
+
+#     # 이름 순으로 정렬
+#     participant.sort()
+#     completion.sort()
+    
+#     # 가장 마지막 사람 초기 조건
+#     answer = participant[-1]
+    
+#     # 참가자 -> 도착 했는지 확인
+#     for i in range(len(completion)):
+        
+#         # 참가자가 완주자에 없는 경우
+#         if participant[i] != completion[i]:
+#             answer = participant[i]
+#             break
