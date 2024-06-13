c653cdbb546de82fcb4b300566b66ab5d28d6026
E0ho <110147055+E0ho@users.noreply.github.com>
2024-06-14 07:05:22 +0900

    [level 3] Title: 정수 삼각형, Time: 45.98 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -10,18 +10,30 @@ def solution(triangle):
     answer = 0
     n = len(triangle)
     
-    # dp (0 패딩 적용)
-    dp = [[0] * (n + 1)]
-    for _ in range(n):
-        dp.append([0] * (n + 1))
+    dp = [[0] * (n + 1)
+         for _ in range(n + 1)]
     
-    # 더 큰 값으로 더하기 (그래프를 좌측으로 붙여 위, 왼쪽 위)
     for i in range(1, n + 1):
         for j in range(1, i + 1):
             dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + triangle[i-1][j-1]
-    
-    # 최종 출력 층 최대 값
-    answer = max(dp[n])
             
+    answer = (max(dp[-1]))
     
     return answer
+
+
+
+#     n = len(triangle)
+    
+#     # dp (0 패딩 적용)
+#     dp = [[0] * (n + 1)]
+#     for _ in range(n):
+#         dp.append([0] * (n + 1))
+    
+#     # 더 큰 값으로 더하기 (그래프를 좌측으로 붙여 위, 왼쪽 위)
+#     for i in range(1, n + 1):
+#         for j in range(1, i + 1):
+#             dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + triangle[i-1][j-1]
+    
+#     # 최종 출력 층 최대 값
+#     answer = max(dp[n])
