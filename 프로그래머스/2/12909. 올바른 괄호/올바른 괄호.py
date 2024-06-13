81a9c0a7a1866622584514f3d2cfd3a5a2e5aee3
E0ho <110147055+E0ho@users.noreply.github.com>
2024-06-14 04:50:07 +0900

    [level 2] Title: 올바른 괄호, Time: 8.27 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,23 +1,24 @@
+from collections import deque
+
 def solution(s):
     answer = True
     
-    count = 0
+    q = deque()
     
-    for c in s:
-        # 괄호 열림
-        if c == '(':
-            count += 1
+    for ele in s:
+        if ele =='(':
+            q.append('(')
+    
+        if ele ==')':
+            if not q:
+                return False
             
-        # 괄호 닫힘
-        else:
-            count -= 1
-        
-        # 열리지 않고 닫힌 경우
-        if count < 0:
-            answer = False
-            return answer
-    # 개수가 맞지 않은 경우
-    if count != 0:
-        answer = False
+            temp = q.popleft()
+            
+            if temp != '(':
+                return False
+            
+    if q:
+        return False
 
     return answer
