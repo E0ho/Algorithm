8a44eaca7db156483077d52376bf5361b1f315db
E0ho <110147055+E0ho@users.noreply.github.com>
2024-06-14 04:30:58 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 81.45 ms, Memory: 28.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -6,17 +6,39 @@ from collections import deque
 def solution(arr):
     answer = []
     
-    # 초기값 (첫번째 원소)
-    pre_value = arr[0]
-    answer.append(pre_value)
+    q = deque(arr)
+    pre_temp = q.popleft()   # 첫번째 원소
     
-    # 모든 원소 검사
-    for now_value in arr:
-        
-        # 중복되지 않는 원소 추가
-        if pre_value != now_value:
-            answer.append(now_value)
-        
-        pre_value = now_value
+    li = []
+    li.append(pre_temp)
+    
+    while q:
+        temp = q.popleft()
+        if temp != pre_temp:
+            li.append(temp)
+            
+        pre_temp = temp
+    
+    answer = li
         
     return answer
+
+
+
+
+
+
+
+#     # 초기값 (첫번째 원소)
+#     arr = deque(arr)
+#     pre_value = arr[0]
+#     answer.append(pre_value)
+    
+#     # 모든 원소 검사
+#     for now_value in arr:
+        
+#         # 중복되지 않는 원소 추가
+#         if pre_value != now_value:
+#             answer.append(now_value)
+        
+#         pre_value = now_value
