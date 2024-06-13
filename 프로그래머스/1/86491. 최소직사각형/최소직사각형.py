6c97ea3f1dc6a2e334beaa61d91f76ff958ee12d
E0ho <110147055+E0ho@users.noreply.github.com>
2024-06-14 06:07:44 +0900

    [level 1] Title: 최소직사각형, Time: 1.61 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -4,19 +4,31 @@
 def solution(sizes):
     answer = 0
     
-    # 가로 길이를 무조건 긴변으로 돌리기
-    li = []
-    for a, b in sizes:
-        li.append( (max(a, b), min(a, b)) )
-        
-    width_li = []
-    height_li = []
+    for size in sizes:
+        size.sort()
     
-    # 가로 세로 배열 만들기
-    for width, height in li:
-        width_li.append(width)
-        height_li.append(height)
+    width = max([x[0] for x in sizes])
+    height = max([x[1] for x in sizes])
     
-    # 각 길이의 최대 값
-    answer = max(width_li) * max(height_li)
+    answer = (width * height)
+
     return answer
+
+
+
+
+#     # 가로 길이를 무조건 긴변으로 돌리기
+#     li = []
+#     for a, b in sizes:
+#         li.append( (max(a, b), min(a, b)) )
+        
+#     width_li = []
+#     height_li = []
+    
+#     # 가로 세로 배열 만들기
+#     for width, height in li:
+#         width_li.append(width)
+#         height_li.append(height)
+    
+#     # 각 길이의 최대 값
+#     answer = max(width_li) * max(height_li)
