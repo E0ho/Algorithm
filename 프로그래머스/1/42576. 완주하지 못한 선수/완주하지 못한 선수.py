4ad63e74a29f816ce2bd1328dbc44ba37cc5140c
E0ho <110147055+E0ho@users.noreply.github.com>
2024-07-17 21:19:06 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 47.87 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,18 +2,25 @@
 # participant : 참여한 선수 이름 리스트
 # completion : 완주한 선수 이름 리스트
 # 완주하지 못한 선수 출력
-from collections import Counter
 
 def solution(participant, completion):
     answer = ''
-    a = Counter(participant)
-    b = Counter(completion)
-
-    a.subtract(b)
     
-    a = list(a.most_common(1))
+    # 참자가 hash
+    participant_dic = {}
+    for name in participant:
+        num = participant_dic.get(name, 0)
+        participant_dic[name] = num + 1
+        
+    # 완주자 제거
+    for name in completion:
+        participant_dic[name] = participant_dic[name] - 1
     
-    answer = a[0][0]
+    # 완주하지 못한 이름 출력
+    for key, value in participant_dic.items():
+        if value != 0:
+            answer += key
+            
     return answer
 
 
