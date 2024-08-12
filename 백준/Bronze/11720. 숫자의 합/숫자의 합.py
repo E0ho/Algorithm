66f35400d170f7794b4477de13649deb58fcc7c4
E0ho <110147055+E0ho@users.noreply.github.com>
2024-08-12 16:04:30 +0900

    [Bronze IV] Title: 숫자의 합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.py
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -1,15 +1,11 @@
-# N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
-# 1. N개 숫자 입력받기
-# 2. 반복문을 활용해 모든 숫자 더하기
-
 # 입력
-n = int(input())
-string = input()
-total = 0
+num = int(input())
+li = input()
 
-# 모든 숫자 합
-for ele in string:
-    total += int(ele)
+# 모든 숫자 합 구하기 -> sum(li)
+sum = 0
+for ch in li:
+    sum += int(ch)
 
 # 출력
-print(total)
+print(sum)
