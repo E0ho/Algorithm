a842c76b16cf6678e0014986a3c310afaeced8f2
E0ho <110147055+E0ho@users.noreply.github.com>
2024-08-12 17:58:45 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 248 ms, Memory: 41144 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,23 +1,18 @@
-# 구간 합 문제
-# 합 배열을 통해 Timeout 방지
-
-# 다중 입력 설정
 import sys
 input = sys.stdin.readline
 
 # 입력
-N, M = tuple(map(int, input().split()))
+n, m = tuple(map(int, input().split()))
 li = list(map(int, input().split()))
 
+# 합 배열
 s = [0]
-temp = 0
-
-# 합 배열 구하기
 for ele in li:
-    temp += ele
-    s.append(temp)
-    
+    s.append(ele + s[-1])
+
 # 구간 합
-for _ in range(M):
+for _ in range(m):
     i, j = map(int, input().split())
-    print(s[j] - s[i-1])
+    
+    # 출력
+    print(s[j] - s[i - 1])
