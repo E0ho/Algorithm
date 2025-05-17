191af30d40fb83e17324c102ae46497cfe8ff66e
E0ho <110147055+E0ho@users.noreply.github.com>
2025-05-17 16:03:30 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,35 +1,11 @@
+# i ~ j (INDEX - 1)
+# K 번째 수 (INDEX - 1)
+
 def solution(array, commands):
     answer = []
-    
-    for command in commands:
-        i = command[0]
-        j = command[1]
-        k = command[2]
-        
-        new_array = array[i-1:j]
-        new_array.sort()
-        answer.append(new_array[k-1])
+    for i, j, k in commands:
+        temp_array = array[i-1:j]
+        temp_array.sort()
+        answer.append(temp_array[k-1])
 
     return answer
-
-
-
-
-
-
-
-    # 명령어 반복
-#     for cmd in commands:
-#         i = cmd[0]
-#         j = cmd[1]
-#         k = cmd[2]
-        
-#         # 배열 슬라이싱
-#         temp_array = array[i-1:j]
-        
-#         # 정렬
-#         temp_array.sort()
-        
-#         # k 번째 원소 추가
-#         answer.append(temp_array[k-1])
-        
