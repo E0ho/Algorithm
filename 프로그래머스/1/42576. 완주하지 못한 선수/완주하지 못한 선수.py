d69fd5e8ce5964f966c60fd9b97303faab853d5d
E0ho <110147055+E0ho@users.noreply.github.com>
2025-05-17 14:49:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.69 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,41 +1,19 @@
-# 한 사람만 완주 x
-# participant : 참여한 선수 이름 리스트
-# completion : 완주한 선수 이름 리스트
-# 완주하지 못한 선수 출력
+# participant : 참가한 선수들의 이름 (100,000)
+# completion : 완주한 선수들의 이름
 
 def solution(participant, completion):
-    answer = ''
+    dic = {}
+    for key in participant:
+        if dic.get(key, False):
+            dic[key] += 1
+        else:
+            dic[key] = 1
     
-    # 참자가 hash
-    participant_dic = {}
-    for name in participant:
-        num = participant_dic.get(name, 0)
-        participant_dic[name] = num + 1
-        
-    # 완주자 제거
-    for name in completion:
-        participant_dic[name] = participant_dic[name] - 1
+    for key in completion:
+        dic[key] -= 1
     
-    # 완주하지 못한 이름 출력
-    for key, value in participant_dic.items():
-        if value != 0:
-            answer += key
-            
+    new_dic = sorted(dic.items(), key = lambda x:x[1], reverse=True)
+    print(new_dic[0][0])
+    answer = new_dic[0][0]
+    
     return answer
-
-
-
-#     # 이름 순으로 정렬
-#     participant.sort()
-#     completion.sort()
-    
-#     # 가장 마지막 사람 초기 조건
-#     answer = participant[-1]
-    
-#     # 참가자 -> 도착 했는지 확인
-#     for i in range(len(completion)):
-        
-#         # 참가자가 완주자에 없는 경우
-#         if participant[i] != completion[i]:
-#             answer = participant[i]
-#             break
