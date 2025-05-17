d9ac8dff0d29b887029922b50d856646749b1884
E0ho <110147055+E0ho@users.noreply.github.com>
2025-05-17 15:54:48 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,23 +1,15 @@
+# 최소 1개 이상 옷 입음
+# 종류별 하나 선택 가능
+
 def solution(clothes):
-    answer = 0
+    dic = {}
     
-    # 해시 사전
-    hash_dict = {}
-    for value, kind in clothes:
-        li = hash_dict.get(kind, [])
-        li.append(value)
-        hash_dict[kind] = li
+    # 각 종류 별, 의상 개수
+    for name, kind in clothes:
+        dic[kind] = dic.get(kind, 0) + 1
     
+    total = 1
+    for cnt in dic.values():
+        total *= (cnt + 1)
     
-    # 각 종류 별 갯수
-    num_dict = {}
-    for key, value in hash_dict.items():
-        num_dict[key] = len(hash_dict[key])
-    
-    # 총 경우의 수
-    count = 1
-    for _, value in num_dict.items():
-        count = count * (value + 1)
-    
-    answer = count - 1   # 모두 선택하지 않은 경우
-    return answer
+    return total - 1
